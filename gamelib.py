7909c82efd6eb6b59dc72f3c4c131597f56462a0
ckhoarmnp <sorapol57@gmail.com>
2014-11-18 11:04:45 +0700

    make gamelib and main

---
+++ gamelib.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame import *
+
+class SimpleGame(object):
+	def __init__:
+		pass
+
+	def init(self):
+		pass
+
+	def run(self):
+		pass
+
+	def update(self):
+		pass
+
+	def render(self):
+		pass
+
